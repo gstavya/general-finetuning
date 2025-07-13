8f1f0dda739e9679d1aab153fc6d617585b8b0dd
gstavya <137587109+gstavya@users.noreply.github.com>
2025-07-13 10:34:03 -0700

    Update yolov11.py

--- yolov11.py
+++ yolov11.py
@@ -686,7 +686,7 @@ def main():
     SOURCE_DATA_CONTAINER = "2ktestsidewalk60"
     CHECKPOINT_CONTAINER = "2ktestyolosidewalk60"
     LOCAL_DATA_DIR = "/mnt/data/yolo_sidewalk"
-    NUM_EPOCHS = 300
+    NUM_EPOCHS = 10
     BATCH_SIZE = 64  # Total batch size across all GPUs
     DEVICE = '0,1,2,3'  # Use 4 GPUs
     SAVE_PERIOD = 5
